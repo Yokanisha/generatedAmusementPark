8979ecdb405767ed6bea0209b5cf380258deae43
Fatih Özkan <fatihzkn@web.de>
2024-04-02 17:06:31 +0000

    add ice cream sed

--- Dbt/amusement-park/models/staging/stg_action.sql
+++ Dbt/amusement-park/models/staging/stg_action.sql
@@ -9,12 +9,12 @@ source as (
 renamed as (
 
     select
-        identification_card_id,
-        ice_cream_parlor,
-        french_fries,
-        drink,
-        souvenir,
-        __index_level_0__
+        cast(identification_card_id AS INTEGER) AS identification_card_id,
+        cast(ice_cream_parlor AS INTEGER) AS ice_cream_parlor_id,
+        cast(french_fries AS INTEGER) AS french_fries_id,
+        cast(drink AS INTEGER) AS drink_id,
+        cast(souvenir AS INTEGER) AS souvenir_id,
+        cast( __index_level_0__ AS INTEGER) AS index_nr
 
     from source
 
